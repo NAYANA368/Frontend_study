d4eaf6fccce945685ab0ee67b129b4bbd9760c0a
nayana <nayanakrishnank2016@gmail.com>
2026-01-18 10:26:16 +0530

    new_files

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// let name="nayana krishnan";
+// document.write(name.length);
+
+let s="Hello world Hello world ";
+document.write(s.length);
+document.write("<br>");
+document.write(s.replace("Hello","hi"));
+document.write("<br>");
+document.write(s.replaceAll("Hello","hi"));
+document.write("<br>");
+document.write(s.replace(/Hello/g,"a"))
+document.write("<br>");
+document.write(s.indexOf("Hello",5));
+document.write("<br>");
+document.write(Math.E+"<br>");
+ document.write(Math.PI+"<br>"); 
+ document.write(Math.SQRT2+"<br>"); 
+ document.write(Math.SQRT1_2+"<br>");
+ document.write(Math.LN2+"<br>");
+ document.write(Math.LN10+"<br>");
+ document.write(Math.LOG2E+"<br>");
+ document.write(Math.LOG10E+"<br>"); 
+ document.write(Math.ceil(-10.99)+"<br>");
